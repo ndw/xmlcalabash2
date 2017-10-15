7679e2e525d0fd39143e1f1a464c6396684c82d9
Norman Walsh <ndw@nwalsh.com>
2017-10-15 17:16:15 -0500

    Remove one-off test harness

--- src/test/scala/com/xmlcalabash/test/RunSpecificTestSpec.scala
+++
@@ -1,7 +0,0 @@
-package com.xmlcalabash.test
-
-import com.xmlcalabash.testers.XProcTestSpec
-
-class RunSpecificTestSpec extends XProcTestSpec {
-  runtests("Run the extensions test suite", "src/test/resources/test-suite/test-suite/tests/p-count-001.xml")
-}
