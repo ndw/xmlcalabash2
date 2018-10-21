ee4addfdf382ab5af6c866f2d9ea4d33375207c7
Norman Walsh <ndw@nwalsh.com>
2018-10-21 16:23:27 -0500

    Improved p:cast-content-type

--- src/main/scala/com/xmlcalabash/steps/CastContentType.scala
+++ src/main/scala/com/xmlcalabash/steps/CastContentType.scala
@@ -1,16 +1,17 @@
 package com.xmlcalabash.steps
 
-import java.io.ByteArrayOutputStream
+import java.io.{ByteArrayInputStream, ByteArrayOutputStream}
 import java.net.URI
 import java.util.Base64
 
 import com.jafpl.messages.Message
+import com.xmlcalabash.config.DocumentRequest
 import com.xmlcalabash.exceptions.XProcException
 import com.xmlcalabash.messages.XdmValueItemMessage
 import com.xmlcalabash.model.util.{SaxonTreeBuilder, XProcConstants}
 import com.xmlcalabash.runtime.{ExpressionContext, StaticContext, XProcMetadata, XProcXPathExpression, XmlPortSpecification}
-import com.xmlcalabash.util.{MediaType, S9Api, ValueUtils}
-import net.sf.saxon.s9api.{QName, XdmItem, XdmNode, XdmValue}
+import com.xmlcalabash.util.{MediaType, S9Api, TypeUtils, ValueUtils}
+import net.sf.saxon.s9api.{QName, Serializer, XdmItem, XdmNode, XdmValue}
 
 import scala.collection.mutable
 
@@ -18,6 +19,7 @@ class CastContentType() extends DefaultXmlStep {
   private var item = Option.empty[Any]
   private var metadata = Option.empty[XProcMetadata]
   private var castTo = MediaType.OCTET_STREAM
+  private var parameters = Option.empty[XdmValue]
 
   override def inputSpec: XmlPortSpecification = XmlPortSpecification.ANYSOURCE
   override def outputSpec: XmlPortSpecification = XmlPortSpecification.ANYRESULT
@@ -28,8 +30,12 @@ class CastContentType() extends DefaultXmlStep {
   }
 
   override def receiveBinding(variable: QName, value: XdmValue, context: ExpressionContext): Unit = {
-    if (variable == XProcConstants._content_type) {
-      castTo = MediaType.parse(ValueUtils.singletonStringValue(value, context.location))
+    variable match {
+      case XProcConstants._content_type =>
+        castTo = MediaType.parse(ValueUtils.singletonStringValue(value, context.location))
+      case XProcConstants._parameters =>
+        parameters = Some(value)
+      case _ => Unit
     }
   }
 
@@ -38,6 +44,10 @@ class CastContentType() extends DefaultXmlStep {
 
     if (castTo.xmlContentType) {
       castToXML(context)
+    } else if (castTo.jsonContentType) {
+      castToJSON(context)
+    } else if (castTo.htmlContentType) {
+      castToHTML(context)
     } else if (castTo.textContentType) {
       castToText(context)
     } else {
@@ -48,25 +58,15 @@ class CastContentType() extends DefaultXmlStep {
   def castToXML(context: StaticContext): Unit = {
     val contentType = metadata.get.contentType
 
-    if (contentType.xmlContentType) {
+    if (contentType.xmlContentType || contentType.htmlContentType) {
       consumer.get.receive("result", item.get, new XProcMetadata(castTo, metadata.get.properties))
-    } else if (castTo.textContentType) {
-      val builder = new SaxonTreeBuilder(config)
-
-      val baseURI = if (metadata.get.properties.contains(XProcConstants._base_uri)) {
-        Some(new URI(S9Api.valuesToString(metadata.get.properties(XProcConstants._base_uri))))
-      } else {
-        None
-      }
-
-      builder.startDocument(baseURI)
-      builder.addStartElement(XProcConstants.c_data)
-      builder.addAttribute(XProcConstants._content_type, contentType.toString)
-      builder.startContent()
-      builder.addText(item.get.asInstanceOf[XdmNode].getStringValue)
-      builder.addEndElement()
-      builder.endDocument()
-      consumer.get.receive("result", builder.result, new XProcMetadata(castTo, metadata.get.properties))
+    } else if (contentType.textContentType) {
+      val text = item.asInstanceOf[XdmNode].getStringValue
+      val bais = new ByteArrayInputStream(text.getBytes("UTF-8"))
+      val baseURI = metadata.get.baseURI.getOrElse(new URI(""))
+      val req = new DocumentRequest(baseURI, contentType)
+      val resp = config.documentManager.parse(req, bais)
+      consumer.get.receive("result", resp.value, new XProcMetadata(castTo, metadata.get.properties))
     } else if (contentType.jsonContentType) {
       // Step 1, convert the map into a JSON text string
       var expr = new XProcXPathExpression(ExpressionContext.NONE, "serialize($map, map {\"method\": \"json\"})")
@@ -113,29 +113,67 @@ class CastContentType() extends DefaultXmlStep {
   def castToText(context: StaticContext): Unit = {
     val contentType = metadata.get.contentType
 
-    if (contentType.xmlContentType) {
-      val baos = new ByteArrayOutputStream()
-      val serializer = config.config.processor.newSerializer(baos)
-      S9Api.serialize(config.config, item.get.asInstanceOf[XdmNode], serializer)
-
-      val builder = new SaxonTreeBuilder(config)
-
-      val baseURI = if (metadata.get.properties.contains(XProcConstants._base_uri)) {
-        Some(new URI(S9Api.valuesToString(metadata.get.properties(XProcConstants._base_uri))))
-      } else {
-        None
+    if (contentType.textContentType) {
+      consumer.get.receive("result", item.get, new XProcMetadata(castTo, metadata.get.properties))
+    } else  if (contentType.xmlContentType || contentType.htmlContentType) {
+      val serialOpts = mutable.HashMap.empty[QName, String]
+      if (parameters.isDefined) {
+        val opts = TypeUtils.castAsScala(parameters.get).asInstanceOf[Map[Any, Any]]
+        for (opt <- opts.keySet) {
+          val value = opts(opt)
+          opt match {
+            case name: QName =>
+              serialOpts.put(name, opt.toString)
+            case name: String =>
+              if (!name.contains(":")) {
+                serialOpts.put(new QName(name), opt.toString)
+              }
+          }
+        }
       }
 
-      builder.startDocument(baseURI)
-      builder.addText(baos.toString)
-      builder.addEndElement()
+      val stream = new ByteArrayOutputStream()
+      val serializer = config.processor.newSerializer(stream)
+
+      S9Api.configureSerializer(serializer, config.defaultSerializationOptions(contentType))
+      S9Api.configureSerializer(serializer, serialOpts.toMap)
+
+      S9Api.serialize(config.config, item.asInstanceOf[XdmNode], serializer)
+
+      val builder = new SaxonTreeBuilder(config)
+      builder.startDocument(metadata.get.baseURI)
+      builder.addText(stream.toString("UTF-8"))
       builder.endDocument()
       consumer.get.receive("result", builder.result, new XProcMetadata(castTo, metadata.get.properties))
-    } else if (castTo.textContentType) {
-      consumer.get.receive("result", item.get, new XProcMetadata(castTo, metadata.get.properties))
+    } else if (contentType.jsonContentType) {
+      var expr = new XProcXPathExpression(ExpressionContext.NONE, "serialize($map, map {\"method\": \"json\"})")
+      val bindingsMap = mutable.HashMap.empty[String, Message]
+      var vmsg = new XdmValueItemMessage(item.get.asInstanceOf[XdmItem], XProcMetadata.XML, ExpressionContext.NONE)
+      bindingsMap.put("{}map", vmsg)
+      var smsg = config.expressionEvaluator.singletonValue(expr, List(), bindingsMap.toMap, None)
+
+      val builder = new SaxonTreeBuilder(config)
+      builder.startDocument(metadata.get.baseURI)
+      builder.addText(smsg.item.toString)
+      builder.endDocument()
+      consumer.get.receive("result", builder.result, new XProcMetadata(castTo, metadata.get.properties))
     } else {
-      // FIXME: Must handle the c:data case...
-      throw XProcException.stepError(1003)
+      val builder = new SaxonTreeBuilder(config)
+      builder.startDocument(metadata.get.baseURI)
+      builder.addStartElement(XProcConstants.c_data)
+      builder.addAttribute(XProcConstants._content_type, contentType.toString)
+      builder.addAttribute(XProcConstants._encoding, "base64")
+      builder.startContent()
+
+      // The string may contain CRLF line endings, remove the CRs
+      val base64str = Base64.getMimeEncoder.encodeToString(item.get.asInstanceOf[Array[Byte]]).replace("\r", "")
+      builder.addText(base64str)
+
+      builder.addEndElement()
+      builder.endDocument()
+
+      val doc = builder.result
+      consumer.get.receive("result", doc, new XProcMetadata(castTo, metadata.get.properties))
     }
   }
 
@@ -144,4 +182,8 @@ class CastContentType() extends DefaultXmlStep {
     throw new UnsupportedOperationException("Casting to JSON hasn't been implemented")
   }
 
+  def castToHTML(context: StaticContext): Unit = {
+    val contentType = metadata.get.contentType
+    throw new UnsupportedOperationException("Casting to HTML hasn't been implemented")
+  }
 }
